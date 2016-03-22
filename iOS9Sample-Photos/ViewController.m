8a146575a081ee2b33a142e95827392c2633c7c6
MJ Lee <mj@MJ-Lees-MacBook.lan>
2016-03-22 15:01:18 +0800

    调整逻辑细节
    调整逻辑细节

--- iOS9Sample-Photos/ViewController.m
+++ iOS9Sample-Photos/ViewController.m
@@ -21,97 +21,34 @@ static NSString * const XMGCollectionName = @"小码哥-Photos";
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    [self searchAllImages];
 }
 
-/**
- * 获得自定义的相册对象
- */
-- (PHAssetCollection *)collection
-{
-    // 先从已存在相册中找到自定义相册对象
-    PHFetchResult<PHAssetCollection *> *collectionResult = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeAlbum subtype:PHAssetCollectionSubtypeAlbumRegular options:nil];
-    for (PHAssetCollection *collection in collectionResult) {
-        if ([collection.localizedTitle isEqualToString:XMGCollectionName]) {
-            return collection;
-        }
-    }
-    
-    // 新建自定义相册
-    __block NSString *collectionId = nil;
-    NSError *error = nil;
-    [[PHPhotoLibrary sharedPhotoLibrary] performChangesAndWait:^{
-        collectionId = [PHAssetCollectionChangeRequest creationRequestForAssetCollectionWithTitle:XMGCollectionName].placeholderForCreatedAssetCollection.localIdentifier;
-    } error:&error];
-    
-    if (error) {
-        NSLog(@"创建相册【%@】失败", XMGCollectionName);
-        return nil;
-    }
-    
-    return [PHAssetCollection fetchAssetCollectionsWithLocalIdentifiers:@[collectionId] options:nil].lastObject;
-}
-
-/**
- * 保存图片到相册
- */
-- (IBAction)saveImage {
-    PHAuthorizationStatus status = [PHPhotoLibrary authorizationStatus];
-    if (status == PHAuthorizationStatusDenied) {
-        NSLog(@"请到【设置-隐私-照片】打开访问开关");
-    } else if (status == PHAuthorizationStatusRestricted) {
-        NSLog(@"无法访问相册");
-    } else {
-        // 保存相片的标识
-        __block NSString *assetId = nil;
-        
-        [[PHPhotoLibrary sharedPhotoLibrary] performChanges:^{
-            // 保存相片到相机胶卷，并返回标识
-            assetId = [PHAssetCreationRequest creationRequestForAssetFromImage:[UIImage imageNamed:@"logo"]].placeholderForCreatedAsset.localIdentifier;
-        } completionHandler:^(BOOL success, NSError * _Nullable error) {
-            if (!success) {
-                NSLog(@"保存失败：%@", error);
-                return;
-            }
-            
-            // 根据标识获得相片对象
-            PHAsset *asset = [PHAsset fetchAssetsWithLocalIdentifiers:@[assetId] options:nil].lastObject;
-            
-            // 拿到自定义的相册对象
-            PHAssetCollection *collection = [self collection];
-            if (collection == nil) return;
-            
-            // 保存相片到自定义相册中
-            [[PHPhotoLibrary sharedPhotoLibrary] performChanges:^{
-                [[PHAssetCollectionChangeRequest changeRequestForAssetCollection:collection] addAssets:@[asset]];
-            } completionHandler:^(BOOL success, NSError * _Nullable error) {
-                if (success) {
-                    NSLog(@"保存成功");
-                } else {
-                    NSLog(@"保存失败：%@", error);
-                }
-            }];
-        }];
-    }
-}
-
+#pragma mark - 查询相册中的图片
 /**
  * 查询所有的图片
  */
 - (IBAction)searchAllImages {
-    
-    // 遍历所有的自定义相册
-    PHFetchResult<PHAssetCollection *> *collectionResult0 = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeAlbum subtype:PHAssetCollectionSubtypeAlbumRegular options:nil];
-    for (PHAssetCollection *collection in collectionResult0) {
-        [self searchAllImagesInCollection:collection];
-    }
-    
-    // 获得相机胶卷的图片
-    PHFetchResult<PHAssetCollection *> *collectionResult1 = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeSmartAlbum subtype:PHAssetCollectionSubtypeAlbumRegular options:nil];
-    for (PHAssetCollection *collection in collectionResult1) {
-        if (![collection.localizedTitle isEqualToString:@"Camera Roll"]) continue;
-        [self searchAllImagesInCollection:collection];
-        break;
-    }
+    // 判断授权状态
+    [PHPhotoLibrary requestAuthorization:^(PHAuthorizationStatus status) {
+        if (status != PHAuthorizationStatusAuthorized) return;
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            // 遍历所有的自定义相册
+            PHFetchResult<PHAssetCollection *> *collectionResult0 = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeAlbum subtype:PHAssetCollectionSubtypeAlbumRegular options:nil];
+            for (PHAssetCollection *collection in collectionResult0) {
+                [self searchAllImagesInCollection:collection];
+            }
+            
+            // 获得相机胶卷的图片
+            PHFetchResult<PHAssetCollection *> *collectionResult1 = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeSmartAlbum subtype:PHAssetCollectionSubtypeAlbumRegular options:nil];
+            for (PHAssetCollection *collection in collectionResult1) {
+                if (![collection.localizedTitle isEqualToString:@"Camera Roll"]) continue;
+                [self searchAllImagesInCollection:collection];
+                break;
+            }
+        });
+    }];
 }
 
 /**
@@ -135,8 +72,77 @@ static NSString * const XMGCollectionName = @"小码哥-Photos";
         CGSize targetSize = CGSizeMake(asset.pixelWidth, asset.pixelHeight);
         // 请求图片
         [[PHImageManager defaultManager] requestImageForAsset:asset targetSize:targetSize contentMode:PHImageContentModeDefault options:imageOptions resultHandler:^(UIImage * _Nullable result, NSDictionary * _Nullable info) {
-            NSLog(@"图片：%@", result);
+            NSLog(@"图片：%@ %@", result, [NSThread currentThread]);
         }];
     }
 }
+
+#pragma mark - 保存图片到自定义相册
+/**
+ * 获得自定义的相册对象
+ */
+- (PHAssetCollection *)collection
+{
+    // 先从已存在相册中找到自定义相册对象
+    PHFetchResult<PHAssetCollection *> *collectionResult = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeAlbum subtype:PHAssetCollectionSubtypeAlbumRegular options:nil];
+    for (PHAssetCollection *collection in collectionResult) {
+        if ([collection.localizedTitle isEqualToString:XMGCollectionName]) {
+            return collection;
+        }
+    }
+    
+    // 新建自定义相册
+    __block NSString *collectionId = nil;
+    NSError *error = nil;
+    [[PHPhotoLibrary sharedPhotoLibrary] performChangesAndWait:^{
+        collectionId = [PHAssetCollectionChangeRequest creationRequestForAssetCollectionWithTitle:XMGCollectionName].placeholderForCreatedAssetCollection.localIdentifier;
+    } error:&error];
+    
+    if (error) {
+        NSLog(@"获取相册【%@】失败", XMGCollectionName);
+        return nil;
+    }
+    
+    return [PHAssetCollection fetchAssetCollectionsWithLocalIdentifiers:@[collectionId] options:nil].lastObject;
+}
+
+/**
+ * 保存图片到相册
+ */
+- (IBAction)saveImage {
+    // 判断授权状态
+    [PHPhotoLibrary requestAuthorization:^(PHAuthorizationStatus status) {
+        if (status != PHAuthorizationStatusAuthorized) return;
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSError *error = nil;
+    
+            // 保存相片到相机胶卷
+            __block PHObjectPlaceholder *createdAsset = nil;
+            [[PHPhotoLibrary sharedPhotoLibrary] performChangesAndWait:^{
+                createdAsset = [PHAssetCreationRequest creationRequestForAssetFromImage:[UIImage imageNamed:@"logo"]].placeholderForCreatedAsset;
+            } error:&error];
+            
+            if (error) {
+                NSLog(@"保存失败：%@", error);
+                return;
+            }
+            
+            // 拿到自定义的相册对象
+            PHAssetCollection *collection = [self collection];
+            if (collection == nil) return;
+            
+            [[PHPhotoLibrary sharedPhotoLibrary] performChangesAndWait:^{
+                [[PHAssetCollectionChangeRequest changeRequestForAssetCollection:collection] insertAssets:@[createdAsset] atIndexes:[NSIndexSet indexSetWithIndex:0]];
+            } error:&error];
+            
+            if (error) {
+                NSLog(@"保存失败：%@", error);
+            } else {
+                NSLog(@"保存成功");
+            }
+        });
+    }];
+}
+
 @end
